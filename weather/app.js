a374f95499f6bdee30a5d42a51b96dfd0a287917
Makhadmahaska <mahadmahaska@hotmail.com>
2026-01-29 04:30:55 +0100

    added

---
+++ weather/app.js
@@ -0,0 +1,55 @@
+const date = document.querySelector('#date');
+const city = document.querySelector('#city');
+const temp = document.querySelector('#temp');
+const tempImg = document.querySelector('#temp-img');
+const description = document.querySelector('#description');
+const tempMax = document.querySelector('#temp-max');
+const tempMin = document.querySelector('#temp-min');
+const apiKey = "6498cca2617d214c78df11180084ce69";
+
+
+const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+
+let dateObj = new Date();
+let month = monthNames[dateObj.getUTCMonth()];
+let day = dateObj.getUTCDate() - 1;
+let year = dateObj.getUTCFullYear();
+
+date.innerHTML = `${month} ${day}, ${year}`;
+
+
+const app = document.querySelector('.app');
+
+const getWeather = async () => {
+
+    try {
+        const cityName = document.getElementById('search-bar-input').value;    
+        const apiKey = "6498cca2617d214c78df11180084ce69";
+        const weatherDataFetch = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`,
+    {
+        headers: {
+            Accept: "application/json"
+        }
+    }
+);
+
+       
+
+
+    
+        const weatherData = await weatherDataFetch.json();    
+        console.log(weatherData)    
+        city.innerHTML = `${weatherData.name}`;
+        description.innerHTML = `${weatherData.weather[0].main}`
+        tempImg.innerHTML = `<img src="http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png"/>`;
+        temp.innerHTML = `<h2>${Math.round(weatherData.main.temp)}°C</h2>`;    
+        tempMax.innerHTML = `${weatherData.main.temp_max}°C`;
+        tempMin.innerHTML = `${weatherData.main.temp_min}°C`;
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
